060146a23782e0179134bdee4ae82e06314dd302
WilDev <wilfriedeugster@orange.fr>
2026-04-17 19:26:10 +0200

    feat: limited amount of poems per page

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -14,20 +14,33 @@
     </div>
     <div id="poem-gallery">
       <PoemCard
-        v-for="poem in filteredPoemsData"
+        v-for="poem in paginatedPoemsData"
         :key="poem.title"
         :title="poem.title"
         :author="poem.author"
         :linecount="poem.linecount"
       />
     </div>
+    <div v-if="totalPages > 1" class="pagination">
+      <button :disabled="currentPage === 1" @click="goToPreviousPage">
+        Previous
+      </button>
+      <p class="pagination-status">
+        Page {{ currentPage }} / {{ totalPages }}
+      </p>
+      <button :disabled="currentPage === totalPages" @click="goToNextPage">
+        Next
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed, onMounted, watch } from 'vue'
 import PoemCard from './PoemCard.vue'
 import { fetchShakespearePoems } from '../service/poemsApi'
+
+const poemsPerPage = 18
 
 const poemsData = ref([])
 
@@ -35,6 +48,7 @@ const minLineCount = ref(0)
 const maxLineCount = ref(0)
 
 const searchByTitle = ref('')
+const currentPage = ref(1)
 
 const syncLineCountBounds = (poems) => {
   const availableLineCounts = poems
@@ -62,6 +76,37 @@ const filteredPoemsData = computed(() => {
   return result
 })
 
+const totalPages = computed(() => {
+  return Math.max(1, Math.ceil(filteredPoemsData.value.length / poemsPerPage))
+})
+
+const paginatedPoemsData = computed(() => {
+  const startIndex = (currentPage.value - 1) * poemsPerPage
+  return filteredPoemsData.value.slice(startIndex, startIndex + poemsPerPage)
+})
+
+const goToPreviousPage = () => {
+  if (currentPage.value > 1) {
+    currentPage.value -= 1
+  }
+}
+
+const goToNextPage = () => {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value += 1
+  }
+}
+
+watch([searchByTitle, minLineCount, maxLineCount], () => {
+  currentPage.value = 1
+})
+
+watch(totalPages, (newTotalPages) => {
+  if (currentPage.value > newTotalPages) {
+    currentPage.value = newTotalPages
+  }
+})
+
 onMounted(async () => {
   poemsData.value = await fetchShakespearePoems()
   syncLineCountBounds(poemsData.value)
@@ -79,6 +124,19 @@ onMounted(async () => {
   display: grid;
   grid-template-columns: repeat(3, minmax(0, 1fr));
   gap: 12px;
+}
+
+.pagination {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 12px;
+  margin-top: 20px;
+  flex-wrap: wrap;
+}
+
+.pagination-status {
+  margin: 0;
 }
 
 @media (max-width: 600px) {
